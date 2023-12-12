a1d71715213e426f094bc2902d9580fd59130193
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:00:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä sana: ");
+        string sana = Console.ReadLine();
+
+        if (sana.Length >= 2)
+        {
+            char[] sanaTaulukko = sana.ToCharArray();
+
+            // Vaihdetaan ensimmäinen ja viimeinen kirjain
+            char ensimmäinenKirjain = sanaTaulukko[0];
+            sanaTaulukko[0] = sanaTaulukko[sanaTaulukko.Length - 1];
+            sanaTaulukko[sanaTaulukko.Length - 1] = ensimmäinenKirjain;
+
+            string uusiSana = new string(sanaTaulukko);
+            Console.WriteLine(uusiSana);
+        }
+        else
+        {
+            Console.WriteLine("Syötä vähintään kaksi kirjainta sisältävä sana.");
+        }
+    }
+}
